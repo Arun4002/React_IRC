c172714271a0a0edcc209ce899dae96145ffc0aa
Arun4002 <hariarun1112@gmail.com>
2023-12-13 21:15:43 +0530

    Furniture_Store(CC1)

---
+++ ReactProject/myapp/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import LoginForm from '../../../../Furniture_Store/sampleapp/src/Components/LoginForm';
+// import Signup from './Components/SignUp';
+import './Assets/SiguUp.css';
+ 
+function App() {
+  return (
+    <div className="App">
+      <LoginForm/>
+    </div>
+  );
+}
+
+export default App;
